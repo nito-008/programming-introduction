757de0b5c7c898970863095e2f1ee0a5ce2beff0
nito-008 <100199931+nito-008@users.noreply.github.com>
2026-02-15 13:55:48 +0900

    D: week5

---
+++ D/week5/09-03-ex1/09-03-Rendering.c
@@ -0,0 +1,46 @@
+﻿// Keisanki Joron 2 (Introduction to Computing II)
+// Dept. of Engineering Systems, University of Tsukuba
+// [UTF-8 / Unix]
+// 計算機序論２・実習 (筑波大学工学システム学類)
+
+// 2012/11/19a kameda[at]iit.tsukuba.ac.jp
+// 09.03. 透視投影下での回転と移動
+
+#include "ic2-CommonHeaders.h"
+
+// ***********************************************************************
+// Rendering *************************************************************
+
+// +++--------------------------------------------------
+// スクリーンに描画する
+// +++--------------------------------------------------
+
+// +----------------------------------------------------
+// １フレーム分の描画
+// +----------------------------------------------------
+void ic2_DrawFrame(void)
+{
+  // (1) 描画バッファの初期化
+  // 以前にglClearColor()で指定した色で塗り潰す
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // (2) カメラの設定
+  ic2_SetUpCamera_Perspective(); // 透視投影
+
+  // (3) 光源の設置
+
+  // (4) 物体の描画
+  glMatrixMode(GL_MODELVIEW);
+  {
+    static float roty = 0.0; // Y軸回りの回転 [degree]
+    glLoadIdentity();
+    glTranslatef(0.0, 0.0, -3.0); // 透視投影の描画範囲と対応必要
+    glRotatef(roty, -1, 0, 0);    // X軸回りの回転
+    ic2_OpenGLLogo(logoscale);
+    roty -= 1.0; // 1 [degree] ずつ
+    if (roty <= -360.0)
+      roty += 360.0;
+  }
+  // (5) 描画バッファの切替
+  glutSwapBuffers();
+}
